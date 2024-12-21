b344d9f1907b05d8de7abc5b7ed58b0f01c498d3
Ronald <ronald.alfredo.hb@gmail.com>
2024-12-21 01:33:03 +0000

    Project Start

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+void identificar_enlaces_simbolicos(const char *directorio) {
+    DIR *dir;
+    struct dirent *ent;
+    struct stat st;
+
+    if ((dir = opendir(directorio)) != NULL) {
+        while ((ent = readdir(dir)) != NULL) {
+            char path[1024];
+            snprintf(path, sizeof(path), "%s/%s", directorio, ent->d_name);
+
+            if (lstat(path, &st) == 0) {
+                if (S_ISLNK(st.st_mode)) {
+                    printf("%s es un enlace simbólico\n", path);
+                }
+            } else {
+                perror("lstat");
+            }
+        }
+        closedir(dir);
+    } else {
+        perror("opendir");
+    }
+}
+
+int main() {
+    char directorio[] = "/ruta/a/tu/directorio"; // Reemplaza con la ruta del directorio a analizar
+    identificar_enlaces_simbolicos(directorio);
+    return 0;
+}
