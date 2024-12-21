099919a2966ac906f09022f7043a6afb2934761f
Brhandon Palomo <brhandon28@gmail.com>
2024-12-21 14:57:11 -0400

    funciones de comprobacion para los argumentos

--- main.c
+++ main.c
@@ -54,6 +54,27 @@ int es_directorio_valido(const char *ruta) {
     }
 }
 
+int numero_de_hilos_validos(char *argv, const char *endptr, long num){
+    if (errno == ERANGE) { // Overflow o underflow
+        fprintf(stderr, "Error: El número está fuera del rango representable.\n");
+        return 0;
+    }
+    else if (endptr == argv[2]) { // No se pudo convertir ningún carácter
+        fprintf(stderr, "Error: No se ingresó un número válido.\n");
+        return 0;
+    }
+    else if (*endptr != '\0') { // Caracteres no numéricos después del número
+        fprintf(stderr, "Error: Caracteres no numéricos en el segundo argumento.\n");
+        return 0;
+    }
+    else if(num < 1){
+        printf("El numero debe ser entero positivo\n");
+        return 0;
+    }
+    
+    return 1;  
+}
+
 // ./duplicados -t <numero de threads> -d <directorio de inicio> -m <e | l >
 void identificar_argumentos(int argc, char *argv[]){
 
@@ -76,23 +97,7 @@ void identificar_argumentos(int argc, char *argv[]){
     //Comprobacion tercer argumento
     char *endptr;
     long num = strtol(argv[2], &endptr, 10);
-    if (errno == ERANGE) { // Overflow o underflow
-        fprintf(stderr, "Error: El número está fuera del rango representable.\n");
-        exit(1);
-    }
-
-    if (endptr == argv[2]) { // No se pudo convertir ningún carácter
-        fprintf(stderr, "Error: No se ingresó un número válido.\n");
-        exit(1);
-    }
-
-    if (*endptr != '\0') { // Caracteres no numéricos después del número
-        fprintf(stderr, "Error: Caracteres no numéricos en el segundo argumento.\n");
-        exit(1);
-    }
-
-    if(num < 1){
-        printf("El numero debe ser entero positivo\n");
+    if(numero_de_hilos_validos(argv[2], endptr, num) == 0){
         exit(1);
     }
 
@@ -103,7 +108,19 @@ void identificar_argumentos(int argc, char *argv[]){
     }
 
     //Comprobacion quinto argumento
-    if(es_directorio_valido(argv[4]) != 1){
+    if(es_directorio_valido(argv[4]) == 0){
+        exit(1);
+    }
+
+    //Comprobacion sexto argumento
+    if(strcmp(argv[5], "-m") != 0){
+        printf("Usa: %s -t %ld -d %s -m <e | l >\n", argv[0], num, argv[4]);
+        exit(1);
+    }
+
+    //Comprobacion septimo argumento
+    if((strcmp(argv[6], "e") != 0) && (strcmp(argv[6], "l") != 0)){
+        printf("Usa: %s -t %ld -d %s -m <e | l >\n", argv[0], num, argv[4]);
         exit(1);
     }
 }
