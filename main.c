b9e48bacc3b1967b8dd6648b448ff071edf806f8
Brhandon Palomo <brhandon28@gmail.com>
2024-12-21 22:03:56 -0400

    main con struct global

--- main.c
+++ main.c
@@ -1,9 +1,33 @@
+#include <stdio.h>
 #include "argumentos.h"
 #include "enlaces.h"
 
+struct lista_visitado {
+    char *archivo;
+    char hash[33];
+} *lista_visitado;
+
+char *duplicados;
+
+void comparar_hash() {
+    int cont = 0;
+    for(int i = 0; i < (strlen(lista_visitado) + 1); i++) {
+        for(int j = 0; j < (strlen(lista_visitado) + 1); j++) {
+            if(i != j) {
+                if(lista_visitado[i].hash == lista_visitado[j].hash) {
+                    duplicados[cont] = lista_visitado[j].archivo;
+                    cont++;
+                }
+            }
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     identificar_argumentos(argc, argv);
+    // printf("El tamanio es: %ld", strlen(argv));
     char *directorio = argv[4];
     identificar_enlaces_simbolicos(directorio);
+    comparar_hash();
     return 0;
 }
