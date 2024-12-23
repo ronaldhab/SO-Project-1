e4c067b8cab6201e2b04b8056359b667c6d08728
Brhandon Palomo <brhandon28@gmail.com>
2024-12-23 15:29:33 -0400

    update main

--- main.c
+++ main.c
@@ -1,23 +1,7 @@
 #include <stdio.h>
-#include "argumentos.h"
-#include "operaciones.h"
 #include <string.h>
-
-char *duplicados[];
-
-void comparar_hash() {
-    int cont = 0;
-    for(int i = 0; i < (strlen(cabeza) + 1); i++) {
-        for(int j = 0; j < (strlen(cabeza) + 1); j++) {
-            if(i != j) {
-                if(cabeza[i].valor_hash == cabeza[j].valor_hash) {
-                    duplicados[cont] = cabeza[j].nombre_archivo;
-                    cont++;
-                }
-            }
-        }
-    }
-}
+#include "argumentos.h"
+#include "comparar_hash.h"
 
 int main(int argc, char *argv[]) {
     identificar_argumentos(argc, argv);
