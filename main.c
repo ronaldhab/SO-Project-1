5f6af8539b12f4735c43f88beee82fe6cf6149cd
Brhandon Palomo <brhandon28@gmail.com>
2024-12-23 17:46:37 -0400

    main comentado de momento

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-#include "argumentos.h"
-#include "comparar_hash.h"
+// #include "argumentos.h"
+#include "operaciones.h"
 
 int main(int argc, char *argv[]) {
-    identificar_argumentos(argc, argv);
+    // identificar_argumentos(argc, argv);
     char *directorio = argv[4];
+    obtener_hashes('l');
     return 0;
 }
