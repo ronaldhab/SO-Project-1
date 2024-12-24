1fad66e4af58bc60a208fb23596876104add5eb0
Brhandon Palomo <brhandon28@gmail.com>
2024-12-23 21:38:26 -0400

    main para pruebas

--- main.c
+++ main.c
@@ -7,5 +7,6 @@ int main(int argc, char *argv[]) {
     // identificar_argumentos(argc, argv);
     char *directorio = argv[4];
     insertar_visitados("Juancito", "1234567890");
+    obtener_hashes_exec();
     return 0;
 }
