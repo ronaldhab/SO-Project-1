b2a2235bcce5050e08357cf04d43d7190d2cf520
Brhandon Palomo <brhandon28@gmail.com>
2024-12-24 00:04:00 -0400

    main con valores de prueba

--- main.c
+++ main.c
@@ -6,7 +6,11 @@
 int main(int argc, char *argv[]) {
     // identificar_argumentos(argc, argv);
     char *directorio = argv[4];
-    insertar_visitados("Juancito", "1234567890");
+    insertar_visitados("/home/brhandon/Documentos/SO-Project-1/explora_dir.c", "0");
+    // insertar_visitados("readme.md", NULL);
+    // insertar_visitados("argumentos.c", NULL);
+    // insertar_visitados("operaciones.h", NULL);
+
     obtener_hashes_exec();
     return 0;
 }
