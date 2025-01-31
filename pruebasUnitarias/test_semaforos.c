3716d3aceca77aa0101902bc9c6dbff2cfe8d692
Brhandon Palomo <brhandon28@gmail.com>
2025-01-31 17:18:15 -0400

    Remove commented-out semaphore initialization code in test_semaforos.c

--- pruebasUnitarias/test_semaforos.c
+++ pruebasUnitarias/test_semaforos.c
@@ -2,18 +2,6 @@
 #include <CUnit/Basic.h>
 #include "../semaforos.h"
 
-// sem_t pila_hash_mutex;
-// sem_t visitados_mutex;
-// sem_t coord_hash;
-// sem_t compara_coord;
-
-// void inicializar_semaforos(){
-//     sem_init(&coord_hash, 0, 0);
-//     sem_init(&compara_coord, 0, 0);
-//     sem_init(&pila_hash_mutex, 0, 1);
-//     sem_init(&visitados_mutex, 0, 1);
-// }
-
 void test_inicializar_semaforos(void) {
     inicializar_semaforos();
     int value;
