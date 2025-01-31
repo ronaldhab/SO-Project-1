c8b5f336d99b53e0d2f0773207858cb67ffcf4c9
Brhandon Palomo <brhandon28@gmail.com>
2025-01-31 17:38:24 -0400

    Remove unnecessary semaphore assertions in test_inicializar_semaforos for clarity

--- pruebasUnitarias/test_semaforos.c
+++ pruebasUnitarias/test_semaforos.c
@@ -12,25 +12,13 @@ void test_inicializar_semaforos(void) {
     sem_getvalue(&hash_mutex, &value);
     CU_ASSERT_EQUAL(value, 1);
 
-    sem_getvalue(&compara_mutex, &value);
-    CU_ASSERT_EQUAL(value, 1);
-
     sem_getvalue(&compara_coord, &value);
     CU_ASSERT_EQUAL(value, 0);
-
-    sem_getvalue(&pila_hash_mutex, &value);
-    CU_ASSERT_EQUAL(value, 1);
-
-    sem_getvalue(&visitados_mutex, &value);
-    CU_ASSERT_EQUAL(value, 1);
 
 
     sem_destroy(&coord_hash);
     sem_destroy(&hash_mutex);
     sem_destroy(&compara_coord);
-    sem_destroy(&compara_mutex);
-    sem_destroy(&pila_hash_mutex);
-    sem_destroy(&visitados_mutex);
 }
 
 int main() {
