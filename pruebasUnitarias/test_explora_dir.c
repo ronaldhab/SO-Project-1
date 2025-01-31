81181e905fce0525782325d3571cba3a39116711
Brhandon Palomo <brhandon28@gmail.com>
2025-01-31 17:17:52 -0400

    Add necessary header inclusions in test_explora_dir.c

--- pruebasUnitarias/test_explora_dir.c
+++ pruebasUnitarias/test_explora_dir.c
@@ -6,6 +6,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include "../estructuras.h"
+#include "../operaciones_estructuras.h"
+#include "../obtener_hashes.h"
+
 
 /*Contador provisional*/
 int count = 0;
